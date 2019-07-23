6d7d5f8c5a1b943fd321d1e76d52029e2a2c2102
ripee <ripee.dev@gmail.com>
2019-07-23 13:56:28 +0200

    jfve-common: Use lineageos_jf_defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,7 +51,7 @@ BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_KERNEL_CONFIG := lineageos_jfve_defconfig
+TARGET_KERNEL_CONFIG := lineageos_jf_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
 
 # Audio
